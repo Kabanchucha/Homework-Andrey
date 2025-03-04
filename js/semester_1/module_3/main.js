5bf231eab00e97b13ccc6519fa0cddf17731c1fb
Kabanchucha <cabanchucha@yandex.ru>
2025-03-04 20:52:51 +0300

    selecter-1-module-3

--- js/semester_1/module_3/main.js
+++ js/semester_1/module_3/main.js
@@ -5,3 +5,16 @@ let x2 = 10;
 let y2 = 5;
 let area = Math.abs((x2 - x1) * (y2 - y1) );
 console.log("Площадь:", area)
+
+//Задача 2
+let a = 13.123456789;
+let b = 2.123;
+let n = 5;
+let NormA = Math.round(a * Math.pow(10, n));
+let NormB = Math.round(b * Math.pow(10, n));
+console.log('Исходные числа равны', NormA === NormB);
+console.log('Число A больше числа B', NormA > NormB);
+console.log('Число A меньше числа B', NormA < NormB);
+console.log('Число A больше или равно числу B', NormA >= NormB);
+console.log('Число A меньше или равно числу B', NormA <= NormB);
+console.log('Число A не равно числу B', NormA != NormB);
